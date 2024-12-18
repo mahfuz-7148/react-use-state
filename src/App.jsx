4c7cc71a83a7b0c49aae8ace4c116f074421b2c9
“mahfuz-7148” <mrahman7148@gmail.com>
2024-12-18 10:33:36 +0600

    'up'

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  // const [price, setPrice] = useState(10)
+  // const price = 10
+
+  // const increaseClick = () => {
+  //   price = price + 1
+  // }
+
+  const [myName, setName] = useState('shanto')
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  //  <div>
+  //     <h1>Price: {price} </h1>
+  //     <button onClick={()=> setPrice(price + 1)} className='m-5 bg-green-400 p-3'>Increase</button>
+  //     <button onClick={()=> setPrice(price - 1)} className='m-5 bg-green-400 p-3'>Decrease</button>
+  //     <button onClick={()=> setPrice(0)} className='m-5 bg-green-400 p-3'>Reset</button>
+  //     <h1>Price: {price}</h1>
+  //     <h1>Price: {price}</h1>
+  //  </div>
+
+    <div>
+      <h1>My name is {myName} </h1>
+      <button onClick={()=> setName('mahfuz')} className='m-5 bg-green-400 p-3'>Mahfuz</button>
+    </div>
   )
 }
 
